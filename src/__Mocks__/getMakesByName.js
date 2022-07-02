46003a649d2e428da86c6603104e708371ddf09f
gids-dadi <gideons564@gmail.com>
2022-07-02 11:09:45 +0100

    Remove linter errros

--- src/__Mocks__/getMakesByName.js
+++ src/__Mocks__/getMakesByName.js
@@ -3,7 +3,7 @@ import carImg from '../images/black-car8.png';
 
 function GetMakesByName() {
   const [page, setPage] = useState(0);
-  const [search, setSearch] = useState('');
+  const [setSearch] = useState('');
   let numPages = 0;
   const vehicles = [];
   const numOfpages = Math.floor(vehicles.length / 20);
@@ -17,40 +17,47 @@ function GetMakesByName() {
     setSearch(e.target.value);
   };
 
-  const submitUrl = (e) => {
+  const submitUrl = () => {
     numPages = 2;
   };
 
   return (
-    <div className="child-page">
-      <img src={carImg} alt="" className="big-car-img" />
-      <div className="line" />
+    <div className='child-page'>
+      <img src={carImg} alt='' className='big-car-img' />
+      <div className='line' />
       <h2>Vehicles Makes by Name</h2>
       <p>Enter the name of vehicle</p>
-      <input type="search" onChange={dropvalue} onKeyDown={submitUrl} placeholder="...search" />
-      <section className="container-cards" />
-      <div className="buttons">
+      <input
+        type='search'
+        onChange={dropvalue}
+        onKeyDown={submitUrl}
+        placeholder='...search'
+      />
+      <section className='container-cards' />
+      <div className='buttons'>
         <h4>
-          {page + 1}
-          {' '}
-          of
-          {' '}
-          {numPages}
+          {page + 1} of {numPages}
         </h4>
         <button
-          className="previous"
-          type="button"
+          className='previous'
+          type='button'
           onClick={() => {
-            if (page <= 0) { setPage(numOfpages); } setPage(page - 1);
+            if (page <= 0) {
+              setPage(numOfpages);
+            }
+            setPage(page - 1);
           }}
         >
           previous
         </button>
         <button
-          className="next"
-          type="button"
+          className='next'
+          type='button'
           onClick={() => {
-            if (page >= numOfpages) { setPage(0); } setPage(page + 1);
+            if (page >= numOfpages) {
+              setPage(0);
+            }
+            setPage(page + 1);
           }}
         >
           next page
